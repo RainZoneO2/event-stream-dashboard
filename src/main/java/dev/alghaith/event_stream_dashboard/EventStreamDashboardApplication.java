229b8c9cf73e477b642b2c2f3f587c9d4b3af1df
Ghaith A <alghaith.ah38@gmail.com>
2026-08-11 10:41:42 -0400

    Initial Spring Boot project scaffold via start.spring.io

---
+++ src/main/java/dev/alghaith/event_stream_dashboard/EventStreamDashboardApplication.java
@@ -0,0 +1,13 @@
+package dev.alghaith.event_stream_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventStreamDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alghaith/event_stream_dashboard/EventStreamDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alghaith.event_stream_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStreamDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
